f95cbf76251810a6953dc38de054968a79f88291
jakoismo <shahlarismayilov304@gmail.com>
2024-06-04 01:07:46 +0400

    Areas created succesfully

---
+++ HotelProject.Web/Areas/Admin/ViewComponents/DashboardHeaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Web.Areas.Admin.ViewComponents
+{
+    public class DashboardHeaderViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
